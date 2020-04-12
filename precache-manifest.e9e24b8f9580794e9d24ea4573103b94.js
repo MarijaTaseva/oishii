2ec16dfcd48f9f17d274a58c5d90554a3f77db34
Marija Taseva <tasevamarija@hotmail.com>
2020-04-12 19:08:56 +0200

    Updates

---
+++ precache-manifest.e9e24b8f9580794e9d24ea4573103b94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715fc6368600af7a047e49fcaa449f42",
+    "url": "/oishii/index.html"
+  },
+  {
+    "revision": "b8e1b0907984138c6150",
+    "url": "/oishii/static/js/2.2671ec76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/oishii/static/js/2.2671ec76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae345b09b1f4a63924e",
+    "url": "/oishii/static/js/main.e14e40b4.chunk.js"
+  },
+  {
+    "revision": "627a2909c773c15e41fe",
+    "url": "/oishii/static/js/runtime-main.2f8ce3cc.js"
+  },
+  {
+    "revision": "09ed2051ed0b6e784bede6e70c64e9e7",
+    "url": "/oishii/static/media/mongo.09ed2051.png"
+  },
+  {
+    "revision": "02e04a7848e63b67df6a144278324ab1",
+    "url": "/oishii/static/media/mssql.02e04a78.png"
+  },
+  {
+    "revision": "075d4a52bc887845139850c80a2da667",
+    "url": "/oishii/static/media/net.075d4a52.png"
+  }
+]);
